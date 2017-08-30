684ab7037466cc8e4fbd53d6443fdb3a0a041dfa
Foodie Unicorn <kimwaag@gmail.com>
2017-08-30 16:13:59 -0700

    added single case study in function.php

--- wp-content/themes/accelerate-theme/index.php
+++ wp-content/themes/accelerate-theme/index.php
@@ -15,8 +15,6 @@
  */
 
 get_header(); ?>
-	<!--wp_query object print -->
-<pre><?php print_r($wp_query); exit; ?></pre>
 	<!-- BLOG PAGE -->
 	<section class="index-page">
 		<div class="site-content">
