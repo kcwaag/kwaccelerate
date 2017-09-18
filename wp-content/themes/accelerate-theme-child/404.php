7b4d99f51ad29db2d179059143eb715e5b7e8f54
Foodie Unicorn <kimwaag@gmail.com>
2017-09-17 19:53:15 -0700

    404 page added

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * 404 Page
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+
+get_header()?>
+<section class = "four-o-four clearfix">
+<div id="primary" class="no-page hero-content">
+  <div class="main-content" role="main">
+
+    <article class = "whoops">
+      <h1>Looks like your <span>Acceleration</span> <br>just came to a hard <strong>STOP</strong>.<br> That's not ok!</h1>
+      <h2>You can make a U-turn <a href="<?php echo site_url('/homepage/') ?>">here.</a></h2>
+    </article>
+    </div>
+  </div>
+
+</section>
+
+<?php get_footer(); ?>
